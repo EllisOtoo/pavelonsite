8eb7293663220c60fe9aa4545d21dddb7a3c3c5b
Ellis Otoo <ellisotoo@Elliss-MacBook-Pro.local>
2022-05-23 18:04:19 +0000

    Homepage Image Updated

--- src/components/LogosSection/LogosSection.jsx
+++ src/components/LogosSection/LogosSection.jsx
@@ -51,7 +51,7 @@ function LogosSection({
             //   : "1fr 1fr 1fr",
           }
         }
-        class={`mt-24 w-10/12 px-12 mx-auto grid grid-cols-4 sm:grid-cols-3 gap-12 md:grid-cols-5`}
+        class={`mt-24 w-10/12 px-12 mx-auto grid sm:grid-cols-2 gap-2 md:grid-cols-5`}
       >
         {LogosData.map((item, index) => {
           console.log("idx", index);
