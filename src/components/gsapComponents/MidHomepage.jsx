4243e93b5f888aa70721d0935ebe510c1ea4dedc
Ellis Otoo <ellisotoo@gmail.com>
2022-05-28 18:26:35 +0000

    scrub control imaged added to mid sectiom images

--- src/components/gsapComponents/MidHomepage.jsx
+++ src/components/gsapComponents/MidHomepage.jsx
@@ -11,10 +11,15 @@ function MidHomepage() {
   let topImage, belowImage, rightImageRef, containerNodeRef;
 
   const scrollOptions = {
-    trigger: containerNodeRef,
+    trigger: topImage,
     start: "top 90%",
+    end: "bottom center",
+    // end: "bottom center",
     scrub: true,
-    toggleActions: "play pause resume pause",
+    // toggleActions: "play pause resume pause",
+    // toggleActions: "play none resume pause",
+    toggleActions: "play none none none",
+
     // markers: true,
   };
 
@@ -25,7 +30,7 @@ function MidHomepage() {
     });
     gsap.from(belowImage, {
       scrollTrigger: scrollOptions,
-      y: 300,
+      y: 100,
     });
     gsap.from(rightImageRef, {
       scrollTrigger: scrollOptions,
@@ -36,7 +41,7 @@ function MidHomepage() {
   return (
     <div
       ref={(containerNode) => (containerNodeRef = containerNode)}
-      class="flex    w-full mx-auto aniContainer"
+      class="flex w-full mx-auto aniContainer"
     >
       <div class="flex flex-col">
         <div ref={(node) => (topImage = node)} className="top">
