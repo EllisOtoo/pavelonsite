feac1585f22760c806a084329f1554815e3c5e54
Ellis Otoo <ellisotoo@gmail.com>
2022-11-30 07:32:15 +0000

    header banner removed for start now and sales pages

--- src/screens/ContactSalesAgent/contactSalesAgent.jsx
+++ src/screens/ContactSalesAgent/contactSalesAgent.jsx
@@ -7,14 +7,7 @@ export default () => {
   return (
     <>
       <ResponsiveNav makeMenuDark />
-      <HomeHeroBanner
-        showButtons={false}
-        showImage={false}
-        Caption="Contact Sales Agent"
-        headerGradientClass={"gradient__bg__blue"}
-        SubCaption="Contact us here on:"
-        LeadIn="XXX"
-      />
+
       <div className="w-8/12 m-auto h-72">
         <Typography variant="h4"></Typography>
       </div>
