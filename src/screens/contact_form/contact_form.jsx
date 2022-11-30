2307fed9e3b73e5efa2a1e8582818de3344e40ee
Ellis Otoo <ellisotoo@gmail.com>
2022-11-30 13:54:30 +0000

    Contact form UI updated with MU error states

--- src/screens/contact_form/contact_form.jsx
+++ src/screens/contact_form/contact_form.jsx
@@ -1,5 +1,5 @@
 import { EditSharp } from "@mui/icons-material";
-import { Typography } from "@mui/material";
+import { TextField, Typography } from "@mui/material";
 import { useFormik } from "formik";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
@@ -8,6 +8,7 @@ import ResponsiveNav from "../../components/ResponsiveNav/ResponsiveNav";
 import FooterSection from "../../components/FooterSection/FooterSection";
 import { HomeHeroBanner } from "../../components";
 import { Input, Button } from "@mui/material";
+import InputMask from "react-input-mask";
 
 const client = axios.create({
   baseURL: "https://pavelon-server.herokuapp.com/",
@@ -44,15 +45,15 @@ export default function Contact_form() {
       email: "",
       name: "",
       business_Name: "",
+      phone_Number: "",
     },
     validationSchema: Yup.object({
-      business_Name: Yup.string()
-        .max(15, "Must be 15 characters or less")
-        .required("Business name Required"),
+      business_Name: Yup.string().required("Business name is required"),
       name: Yup.string()
         .max(20, "Must be 20 characters or less")
-        .required("Name Required"),
+        .required("Name is required"),
       email: Yup.string().email("Invalid email address").required("Required"),
+      phone_Number: Yup.string().required("Required"),
     }),
 
     onSubmit: (values, { resetForm }) => {
@@ -63,6 +64,7 @@ export default function Contact_form() {
             email: "",
             name: "",
             business_Name: "",
+            phone_Number: "",
           },
         });
         setShowSuccess(true);
@@ -81,41 +83,86 @@ export default function Contact_form() {
   return (
     <>
       {/* <ResponsiveNav makeMenuDark /> */}
-      <div className="flex flex-col md:flex-row w-8/12 items-center justify-around m-auto min-h-screen">
-        <div className="w-full md:w-6/12">
+      <div className="flex flex-col md:flex-row w-8/12  justify-between items-center m-auto h-screen">
+        <div className="w-full md:w-4/12 h-72">
           <Typography variant="h2">Contact Us </Typography>
           <Typography variant="p">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-            quisquam necessitatibus maiores earum, suscipit quasi nemo
-            aspernatur accusamus, magni porro repudiandae. Asperiores odio
-            laboriosam impedit maxime doloribus. Nostrum, maxime animi!{" "}
+            Please fill form with details required and one of our
+            representatives will be with you shortly
           </Typography>
         </div>
-        <div className="w-full md:w-4/12 p-3">
-          <form className="flex flex-col" onSubmit={formik.handleSubmit}>
-            <Input
+        <div className=" flex flex-col justify-center w-full h-full md:w-6/12  p-3">
+          <form className="flex flex-col  h-72" onSubmit={formik.handleSubmit}>
+            <TextField
+              variant="filled"
+              error={formik.touched.name && formik.errors.name}
+              helperText={
+                formik.touched.name && formik.errors.name && formik.errors.name
+              }
               name="name"
               type="text"
-              placeholder="Enter Client Name"
+              placeholder="Name"
               value={formik.values.name}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
             />
-            {formik.touched.name && formik.errors.name ? (
-              <div className="text-red-900">{formik.errors.name}</div>
-            ) : null}
-            <Input
+
+            <InputMask
+              mask={"999-999-9999"}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.phone_Number}
+              error={formik.touched.phone_Number && formik.errors.phone_Number}
+              helperText={
+                formik.touched.phone_Number &&
+                formik.errors.phone_Number &&
+                "Required"
+              }
+            >
+              {(inputProps) => (
+                <TextField
+                  {...inputProps}
+                  name="phone_Number"
+                  placeholder="Phone number"
+                  variant="filled"
+                  type="text"
+                />
+              )}
+            </InputMask>
+
+            {/* {formik.touched.name && formik.errors.name ? (
+              <div className="text-red-900 px-2">{formik.errors.name}</div>
+            ) : null} */}
+            <TextField
+              error={formik.touched.email && formik.errors.email}
+              helperText={
+                formik.touched.email &&
+                formik.errors.email &&
+                formik.errors.email
+              }
+              variant="filled"
               name="email"
               type="text"
-              placeholder="Enter Email"
+              placeholder="Email"
               value={formik.values.email}
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
             />
-            {formik.touched.email && formik.errors.email ? (
-              <div className="text-red-900">{formik.errors.email}</div>
-            ) : null}
-            <Input
+            {/*       {formik.touched.email && formik.errors.email ? (
+              <div className="text-red-900 px-2">{formik.errors.email}</div>
+            ) : null} */}
+            <TextField
+              error={
+                formik.touched.business_Name &&
+                formik.errors.business_Name &&
+                true
+              }
+              helperText={
+                formik.touched.business_Name &&
+                formik.errors.business_Name &&
+                formik.errors.business_Name
+              }
+              variant="filled"
               name="business_Name"
               type="text"
               placeholder="Business Name"
@@ -123,9 +170,11 @@ export default function Contact_form() {
               onChange={formik.handleChange}
               onBlur={formik.handleBlur}
             />
-            {formik.touched.business_Name && formik.errors.business_Name ? (
-              <div className="text-red-900">{formik.errors.business_Name}</div>
-            ) : null}
+            {/*   {formik.touched.business_Name && formik.errors.business_Name ? (
+              <div className="text-red-900 px-2">
+                {formik.errors.business_Name}
+              </div>
+            ) : null} */}
 
             <Button type="submit">Submit</Button>
             {showSuccess && <p className="text-green-400">Message Sent!</p>}
